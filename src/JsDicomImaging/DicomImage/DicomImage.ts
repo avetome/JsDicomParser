73d59fac79e99d5f798465b74c27cc5a0671b908
avetome <avetome@gmail.com>
2015-06-04 17:41:33 +0300

    Image pixel data extract from dicomdataset

--- src/JsDicomImaging/DicomImage/DicomImage.ts
+++ src/JsDicomImaging/DicomImage/DicomImage.ts
@@ -1,5 +1,12 @@
 module JsDicomImaging {
     export class DicomImage {
         uid: string;
+        photometricInterpretation: string; // TODO: create photometricInterpretation enum
+        rows: number;
+        columns: number;
+        width: number;
+        height: number;
+        pixelsCount: number;
+        sizeInBytes: number;
     }
 }
--- src/JsDicomImaging/DicomImageExctractor.ts
+++ src/JsDicomImaging/DicomImageExctractor.ts
@@ -1,10 +1,87 @@
 /// <reference path="./DicomImage/DicomImage.ts" />
 /// <reference path="./DicomImage/GrayscaleDicomImage.ts" />
 
-module JsDicomImaging {
-    export class DicomImageExtractor {
-        getDicomImage(dicomDataSet: any) {
+module JsDicomImaging 
+{
+    export class DicomImageExtractor 
+    {
+        getDicomImage(dicomDataSet: any) 
+        {
             console.debug(dicomDataSet);
+            var photometricInterpretation = dicomDataSet.getElementAsString('x00280004');
+            console.debug(photometricInterpretation);
+
+            if (this._isColorImage(photometricInterpretation)) {
+                throw "DicomImageExtractor.getDicomImage: only grayscale images supports yet";
+            }
+
+            var image = new GrayscaleDicomImage();
+            image.photometricInterpretation = photometricInterpretation;
+            image.rows = image.height = dicomDataSet.getElementAsUint16('x00280010');
+            image.columns = image.width = dicomDataSet.getElementAsUint16('x00280011');
+            image.pixelsCount = image.rows * image.columns;
+            image.pixelData = this._extractStoredPixels(dicomDataSet, image.width, image.height);
+
+            return image;
+        }
+
+        private _extractStoredPixels(dataSet: any, width: number, height: number)
+        {
+            return this._extractUncompressedPixels(dataSet, width, height);
+        }
+
+        private _extractUncompressedPixels(dataSet: any, width: number, height: number) 
+        {
+            var pixelFormat = this._getPixelFormat(dataSet);
+
+            if(pixelFormat !== 2) // unsigned 16 bit
+            {
+                throw "DicomImageExtractor:_extractUncompressedPixels: only unsigned 16 bit images supports yet"
+            }
+
+            var pixelDataElement = dataSet.elements["x7fe00010"];
+            var pixelDataOffset = pixelDataElement.offset;
+
+            var numPixels = width * height;
+
+            return new Uint16Array(dataSet.byteArray.buffer, pixelDataElement.offset, numPixels);
+        }
+
+        private _getPixelFormat(dataSet: any)
+        {
+            var pixelRepresentation = dataSet.getElementAsUint16('x00280103');
+            var bitsAllocated = dataSet.getElementAsUint16('x00280100');
+
+            if(pixelRepresentation === 0 && bitsAllocated === 8) 
+            {
+                return 1; // unsigned 8 bit
+            } 
+            else if(pixelRepresentation === 0 && bitsAllocated === 16) 
+            {
+                return 2; // unsigned 16 bit
+            } 
+            else if(pixelRepresentation === 1 && bitsAllocated === 16) 
+            {
+                return 3; // signed 16 bit data
+            }
+        }
+
+        private  _isColorImage(photoMetricInterpretation)
+        {
+            if(photoMetricInterpretation === "RGB" ||
+                photoMetricInterpretation === "PALETTE COLOR" ||
+                photoMetricInterpretation === "YBR_FULL" ||
+                photoMetricInterpretation === "YBR_FULL_422" ||
+                photoMetricInterpretation === "YBR_PARTIAL_422" ||
+                photoMetricInterpretation === "YBR_PARTIAL_420" ||
+                photoMetricInterpretation === "YBR_RCT")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
